975c25eb95d34253cf101941db0d2eea78313357
Mimi L <lim.mimic@gmail.com>
2025-08-06 12:14:30 +0200

    edit: line chart

--- src/pages/components/charts/line-chart.jsx
+++ src/pages/components/charts/line-chart.jsx
@@ -12,7 +12,7 @@ export const LineChart = () => {
 
     const getData = () => {
         const data = [];
-        monthlyRecords.forEach((records, index) => {
+        monthlyRecords.slice(0, new Date().getMonth() + 1).forEach((records, index) => {
             const month = index;
             const incomeTotal = getTotal(records.filter((record) => record.category === "Income"));
             const expenseTotal = getTotal(records.filter((record) => record.category !== "Income"));
@@ -20,13 +20,17 @@ export const LineChart = () => {
             data.push({month: month, income: incomeTotal, expense: `${expenseTotal < 0 ? expenseTotal * -1 : expenseTotal}`});
         });
         console.log(data);
-        return data.slice(0, new Date().getMonth());
+        return data;
     };
 
     const createGraph = async () => {
-        var margin = { top: 20, right: 20, bottom: 50, left: 70 };
-        var width = 500 - margin.left - margin.right;
-        var height = 300 - margin.top - margin.bottom;
+
+        const heightValue = 300;
+        const widthValue = 600;
+
+        var margin = { top: 30, right: 20, bottom: 30, left: 40 };
+        var width = widthValue - margin.left - margin.right;
+        var height = heightValue - margin.top - margin.bottom;
 
         d3.select("#line-chart")
             .select("svg")
@@ -34,14 +38,13 @@ export const LineChart = () => {
 
         var svg = d3.select("#line-chart")
                     .append("svg")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
+                    .attr("id", "chart")
+                    .attr("viewBox", `0 0 ${widthValue} ${heightValue}`)
                     .append("g")
-                    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+                    .attr("transform", `translate(${margin.left}, 0)`);
 
-        var xScale = d3.scaleBand().range([0, width]).domain(data.map((d) => d.month));
-        var yScale = d3.scaleLinear().range([height, 0]).domain([0, d3.max(data, (d) => d.income)]);
-
+        var xScale = d3.scaleBand().range([0, width]).domain(data.map((d) => months[d.month]));
+        var yScale = d3.scaleLinear().range([height, 0]).domain([0, d3.max(data, (d) => d.income < d.expense ? d.expense : d.income)]);
 
         svg.append("g")
             .attr("transform", `translate(0, ${height})`)
@@ -52,7 +55,7 @@ export const LineChart = () => {
 
         var incomeLine = d3.line()
                         .curve(d3.curveBasis)
-                        .x((d) => {return xScale(d.month) + 30})
+                        .x((d) => {return xScale(months[d.month]) + 30})
                         .y((d) => {return yScale(d.income)});
         
         svg.append("path")
@@ -65,7 +68,7 @@ export const LineChart = () => {
 
         var expenseLine = d3.line()
                 .curve(d3.curveBasis)
-                .x((d) => {return xScale(d.month) + 30})
+                .x((d) => {return xScale(months[d.month]) + 30})
                 .y((d) => {return yScale(d.expense)});
 
         svg.append("path")
@@ -88,7 +91,7 @@ export const LineChart = () => {
             .enter()
             .append("rect")
             .attr("class", "bar")
-            .attr("x", (d) => xScale(d.month))
+            .attr("x", (d) => xScale(months[d.month]))
             .attr("y", (d) => 0)
             .attr("width", xScale.bandwidth())
             .attr("height", (d) => height - yScale(d3.max(data, (d) => d.income)))
@@ -96,7 +99,7 @@ export const LineChart = () => {
             .on("mouseover", (event, d) => {
                 const [mx, my] = d3.pointer(event);
                 console.log(mx, my);
-                const tooltipText = `Month: ${months[d.month]}<br>Income Total: ${d.income}<br>Expense Total: ${d.expense}<br>Balance: ${(d.income - d.expense).toFixed(2)}`;
+                const tooltipText = `<b>${months[d.month]}</b><br>Income: € ${d.income}<br>Expense: € ${d.expense}<br>Balance: € ${(d.income - d.expense).toFixed(2)}`;
                 
                 tooltip.style("top", `${my}px`)
                         .style("left", `${mx - 50}px`)

6e0922b43f68e77e24cd1569467cb82845081abe
Mimi L <lim.mimic@gmail.com>
2025-08-14 11:39:46 +0200

    edit: clerk redirect error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { ClerkProvider } from '@clerk/clerk-react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 
 import './App.css';
 import Authentication from './pages/authentication/auth';
@@ -11,34 +11,45 @@ import { Income } from './pages/income/income';
 import { Expense } from './pages/expense/expense';
 import { AllRecords}  from './pages/all-records/all-records';
 import { BackButton } from './back-button/back-button';
+import { useEffect } from 'react';
 
 
 
 const CLERK_PUBLISHABLE_KEY = 'pk_test_d29ydGh5LXR1bmEtMTYuY2xlcmsuYWNjb3VudHMuZGV2JA';
 
 function App() {
-  return (
-    <div className="App">
-        <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
-            <ExpenseRecordContextProvider>
-                <Router basename="/expense-tracker" >
-                    <Routes>
-                        <Route path="/" element={<Authentication />} />
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/income" element={<Income />} />
-                        <Route path="/expense" element={<Expense />} />
-                        <Route path="/all" element={<AllRecords />} />  
-                        <Route path="/monthly" element={<AllRecords />} />
-                        <Route path="/*" element={<Authentication />} />
-                    </Routes>
-                    <BackButton />
-                    <ScrollToTop />
-                    <Navbar />
-                </Router>
-            </ExpenseRecordContextProvider>
-        </ClerkProvider>
-    </div>
-  );
+
+    useEffect(() => {
+        console.log(window.location.pathname);
+        if (window.location.pathname === "/") {
+        window.location.replace("/expense-tracker");
+        }
+    }, [window.location]);
+
+    return (
+        <div className="App">
+            <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+                <ExpenseRecordContextProvider>
+                    <Router basename="/expense-tracker" >
+                        <Routes>
+                            <Route path="/" element={<Authentication />} />
+                            <Route path="/dashboard" element={<Dashboard />} />
+                            <Route path="/income" element={<Income />} />
+                            <Route path="/expense" element={<Expense />} />
+                            <Route path="/all" element={<AllRecords />} />  
+                            <Route path="/monthly" element={<AllRecords />} />
+                            <Route path="/*" element={<Authentication />} />
+                        </Routes>
+                        <BackButton />
+                        <ScrollToTop />
+                        <Navbar />
+                    </Router>
+                </ExpenseRecordContextProvider>
+            </ClerkProvider>
+        </div>
+    );
 }
 
 export default App;
+
+

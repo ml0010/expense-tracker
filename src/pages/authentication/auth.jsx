95583263b474c66b9e5d1a7c9722e961a7146ed7
Mimi L <lim.mimic@gmail.com>
2025-10-17 16:31:35 +0200

    edit: styles

--- src/pages/authentication/auth.jsx
+++ src/pages/authentication/auth.jsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react';
 import { Navigate } from "react-router-dom";
 import "./auth.css";
+import backgroundImage from "../../assets/login-background.jpg"
 
 const PUBLISHABLE_KEY = 'pk_test_d29ydGh5LXR1bmEtMTYuY2xlcmsuYWNjb3VudHMuZGV2JA';
 
@@ -11,8 +12,8 @@ if (!PUBLISHABLE_KEY) {
 export const Authentication = () => {
     return (
         <div className="sign-in-container">
-            <div class="header">
-                <div class="inner-header">
+            <div className="header" style={{backgroundImage: "url(" + backgroundImage + ")"}}>
+                <div className="inner-header">
                     <h1>Expense Tracker</h1>
                     <div>
                         <SignedOut>
@@ -29,11 +30,11 @@ export const Authentication = () => {
                     </div>
                 </div>
                 <div>
-                    <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
                         <defs>
                         <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
                         </defs>
-                        <g class="parallax">
+                        <g className="parallax">
                             <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
                             <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
                             <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />

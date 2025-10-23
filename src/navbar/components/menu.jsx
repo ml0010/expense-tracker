a9b120b28c80df04e1dc1be47249c0a5e2bbb407
Mimi L <lim.mimic@gmail.com>
2025-10-23 04:40:44 +0200

    edit: menu fold/expand

--- src/navbar/components/menu.jsx
+++ src/navbar/components/menu.jsx
@@ -1,6 +1,6 @@
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 import { ChartDonutIcon, ClipboardTextIcon, ListIcon, MoneyIcon, PiggyBankIcon, SignOutIcon, XIcon } from '@phosphor-icons/react'
-import { useContext, useEffect, useRef } from 'react'
+import { useContext } from 'react'
 import { Link, useLocation } from 'react-router-dom';
 import ToggleSwitch from './toggle-switch/toggle-switch'
 import { MenuToggleContext } from '../../contexts/menu-toggle-context';
@@ -9,53 +9,64 @@ export const Menu = () => {
 
    const { showMenu, setShowMenu } = useContext(MenuToggleContext);
 
-   const menuRef = useRef();
-
-   useEffect(() => {
-      let handler = (e)=>{
-         if(menuRef.current && !menuRef.current.contains(e.target)){
-               setShowMenu(false);
-         }
-      };
-      document.addEventListener("mousedown", handler);
-      return() =>{
-         document.removeEventListener("mousedown", handler);
-      }
-   }, [menuRef]);
-
+   
    const currentPage = useLocation().pathname.substring(1);
 
    const { user } = useUser();
    const { signOut } = useClerk();
 
    return (
-      <div className="menu">
+      <div className={`menu ${showMenu && "open"}`}>
          <div className='menu-button'>
             {showMenu ? 
                <XIcon size={30} onClick={() => setShowMenu(!showMenu)}/> : 
                <ListIcon size={30} onClick={() => setShowMenu(!showMenu)}/>
             }
          </div>
-         <div className={`menu-bar ${showMenu ? "open" : ""}`} ref={menuRef}>
+         <div className={`menu-bar ${showMenu && "open"}`}>
             <ToggleSwitch />
-            <h2>Menu</h2>
             <hr className="line" />
             <div className="links">
-               <Link className={`link ${currentPage === "dashboard" ? "active" : ""}`} to="/dashboard" onClick={() => setShowMenu(false)}><ChartDonutIcon size={28} /> Dashboard</Link>
-               <Link className={`link ${currentPage === "income" ? "active" : ""}`} to="/income" onClick={() => setShowMenu(false)}><PiggyBankIcon size={28} />Income</Link>
-               <Link className={`link ${currentPage === "expense" ? "active" : ""}`} to="/expense" onClick={() => setShowMenu(false)}><MoneyIcon size={28} /> Expense</Link>
-               <Link className={`link ${currentPage === "all" ? "active" : ""}`} to="/all" state={{ period: "all" }} onClick={() => setShowMenu(false)}><ClipboardTextIcon size={28} /> All Records</Link>
+               <Link 
+                  className={`link ${currentPage === "dashboard" ? "active" : ""}`} 
+                  to="/dashboard" 
+               >
+                  <ChartDonutIcon size={28} />
+                  <span className={`text ${!showMenu && "hidden"}`}>Dashboard</span>
+               </Link>
+               <Link 
+                  className={`link ${currentPage === "income" ? "active" : ""}`} 
+                  to="/income" 
+               >
+                  <PiggyBankIcon size={28} />
+                  <span className={`text ${!showMenu && "hidden"}`}>Income</span>
+               </Link>
+               <Link 
+                  className={`link ${currentPage === "expense" ? "active" : ""}`} 
+                  to="/expense" 
+               >
+                  <MoneyIcon size={28} />
+                  <span className={`text ${!showMenu && "hidden"}`}>Expense</span>
+               </Link>
+               <Link 
+                  className={`link ${currentPage === "all" ? "active" : ""}`} 
+                  to="/all" 
+                  state={{ period: "all" }} 
+               >
+                  <ClipboardTextIcon size={28} />
+                  <span className={`text ${!showMenu && "hidden"}`}>All Records</span>
+               </Link>
             </div>
             <hr className="line" />
             <div className="user">
                <div className="user-info">
                   <UserButton />
-                  <div className="user-detail">
+                  <div className={`user-detail ${!showMenu && "hidden"}`}>
                         <p><b>{user.fullName}</b></p>
                         <p>{user.primaryEmailAddress.emailAddress}</p>
                   </div>
                </div>
-               <Link className="logout-button" onClick={() => signOut({ redirectUrl: "/expense-tracker" })}><SignOutIcon size={20} /></Link>
+               <Link className={`logout-button ${!showMenu && "hidden"}`} onClick={() => signOut({ redirectUrl: "/expense-tracker" })}><SignOutIcon size={20} /></Link>
             </div>
          </div>
 
--- src/navbar/components/toggle-switch/toggle-switch.js
+++ src/navbar/components/toggle-switch/toggle-switch.js
@@ -2,18 +2,22 @@ import { useContext } from 'react';
 import { DarkModeContext } from '../../../contexts/dark-mode-context';
 import { MoonIcon, SunIcon } from '@phosphor-icons/react';
 import './toggle-switch.css';
+import { MenuToggleContext } from '../../../contexts/menu-toggle-context';
 
 const ToggleSwitch = () => {
 
    const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+   const { showMenu } = useContext(MenuToggleContext);
+   
 
    return (
       <div className='toggle-switch'>
+         {showMenu ? 
          <label className='label' htmlFor='toggle'>
             <div className={`toggle ${darkMode ? "dark-mode" : "light-mode"}`}>
                   <div className="icons">
-                     <SunIcon size={24} />
                      <MoonIcon size={24} />
+                     <SunIcon size={24} />
                   </div>
                   <input
                      id="toggle"
@@ -24,6 +28,23 @@ const ToggleSwitch = () => {
                   />
                </div>
          </label>
+         :
+         <label className='label' htmlFor='toggle'>
+            <div className={`toggle-minimal ${darkMode ? "dark-mode" : "light-mode"}`}>
+                  <div className="icons">
+                     {darkMode ? <MoonIcon size={24} /> : <SunIcon size={24} />} 
+                  </div>
+                  <input
+                     id="toggle"
+                     name="toggle"
+                     type="checkbox"
+                     checked={darkMode}
+                     onChange={toggleDarkMode}
+                  />
+               </div>
+         </label>
+         }
+
       </div>
    );
 };
